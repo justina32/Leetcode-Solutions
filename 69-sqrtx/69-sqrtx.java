805c5020ff18b158b200ba8842820c7e99f19d98
Manyuan Tong <50028440+justina32@users.noreply.github.com>
2022-09-03 19:49:12 -0700

    Time: 2 ms (83.46%), Space: 42.5 MB (6.41%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 1) return x;
+        long left = 0;
+        long right = x;
+        while(left < right - 1){
+            long mid = left + (right - left)/2;
+            if(mid * mid == x) return (int)mid;
+            if(mid * mid < x) {
+                left = mid;
+            }else{
+                right = mid -1;
+            }
+        }
+        //case 1 if we have two elements left 
+        if(right * right <= x) return (int) right;
+        //case 2 only one elements or case 1's eles
+        return (int)left;
+    }
+}
